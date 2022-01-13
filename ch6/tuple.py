7b7b36d2b4154fb6e20f1965410b3a6624b10641
brianjacobpage <brianjacobpage@gmail.com>
2022-01-13 10:17:41 -0800

    Add ch6 folder and tuple iteration code

---
+++ ch6/tuple.py
@@ -0,0 +1,6 @@
+#create a tuple var and print the value of each element with position
+
+myTuple = (1, 2, "pie", "apple", "47Ronnin", (16 * 16))
+
+for i in myTuple:
+    print (myTuple.index(i), i)
