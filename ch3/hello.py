0d8929f32e4fb2d4f6d95633f72b031e54b97b50
brianjacobpage <brianjacobpage@gmail.com>
2022-01-13 16:40:31 -0800

    Stuyy too

--- ch3/hello.py
+++ ch3/hello.py
@@ -1,4 +1,6 @@
+#create a function to run the "Hello world" program
 def hello(name: str) -> None:
     print("hello, " + name)
 
+#call the defined function and pass the name argument "joe"
 hello("joe")
