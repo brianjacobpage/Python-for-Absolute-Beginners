54a34c61565bb3fe60d7c86d2303b45101959c71
brianjacobpage <brianjacobpage@gmail.com>
2022-01-13 16:40:18 -0800

    Stuff

---
+++ ch6/lists.py
@@ -0,0 +1,3 @@
+#lists should, in general, contain elements of the same type, IE either int, str, float, etc.
+#as with tuples, lists start element numbering with "0"
+#changing a list element is referred to as "assignment"
--- ch6/tuple.py
+++ ch6/tuple.py
@@ -3,3 +3,8 @@ myTuple = (1, 2, "pie", "apple", "47Ronnin", (16 * 16))
 
 for i in myTuple:
     print (myTuple.index(i), i)
+
+#Tuples require a comma after an element, even when there is only 1 element, IE myTuple = (100,)
+#except in the case of the last element in a tuple with multiple elements. IE myTuple = (100, 200)
+#you can nest tuples within tuples
+#tuples are immutable, where as lists are mutable
