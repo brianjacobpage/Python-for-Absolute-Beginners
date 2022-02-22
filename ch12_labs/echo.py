ca9cd1727ffe889b47cbee7f9da689b8d3a0ecfd
brianjacobpage <brianjacobpage@gmail.com>
2022-02-21 18:41:38 -0800

    Ch12 Echo

---
+++ ch12_labs/echo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#the long way...
+#response = input("What do you want me to echo? ")
+#print(response)
+
+#the eeeeeasy waaaaaaay...
+print(input("what do you want me to echo biz-snatch? "))
